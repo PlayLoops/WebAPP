b344d2f744c223215dd686ded448b21a797c9b89
tazee <tazee@LAPTOP-JPBT8C2C>
2022-12-17 00:28:16 -0500

    Commit3

--- WebAPP/Program.cs
+++ WebAPP/Program.cs
@@ -1,7 +1,12 @@
+using WebAPP.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<IProductService,ProductService>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
 
 var app = builder.Build();
 
